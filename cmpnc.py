f4658446e4d3d2872304512e7e0d6e80f158cbd6
Deepak Chandan <deepak.chandan.science@gmail.com>
2016-03-28 12:42:27 -0400

    Python 3 compatibility and bug fix
    1. Various print statement changes for python 3
    2. Added a fix for comparing netCDF variables which are masked.

--- cmpnc.py
+++ cmpnc.py
@@ -24,28 +24,28 @@ def check_files_exist(file1, file2=""):
 
 def compare_dimensions(dim1, dim2, verbose):
     assert_(len(dim1), len(dim2), "Number of dimensions different in files")
-    assert_(dim1.keys(), dim2.keys(), "Dimensions in files different")
+    assert_(list(dim1.keys()), list(dim2.keys()), "Dimensions in files different")
     if verbose: 
         print("Comparing Dimensions")
-        print("  Both files have {0} dimensions".format(len(dim1)))
+        print(("  Both files have {0} dimensions".format(len(dim1))))
         
-    for k in dim1.keys():
+    for k in list(dim1.keys()):
         assert_(len(dim1[k]), len(dim2[k]), "Lengths not same for dimension {0}".format(k))
-        if verbose: print("  Dimension {0} same".format(k))
+        if verbose: print(("  Dimension {0} same".format(k)))
 
 
 def compare_variables(vars1, vars2, verbose):
     if verbose: print("Comparing Dimensions")
     assert_(len(vars1), len(vars2), "Number of variables different in files")
-    assert_(vars1.keys(), vars2.keys(), "Variables different in files")
+    assert_(list(vars1.keys()), list(vars2.keys()), "Variables different in files")
 
     failed_vars = []
 
     all_okay = True
 
-    for var in vars1.keys():
+    for var in list(vars1.keys()):
         if verbose:
-            print("  {0}".format(var))
+            print(("  {0}".format(var)))
         var1 = vars1[var]
         var2 = vars2[var]
 
@@ -54,16 +54,28 @@ def compare_variables(vars1, vars2, verbose):
         # Checking 'non-string' variables only
         if (var_dtype != np.dtype('S1')):
             try:
-                # assert(np.allclose(var1[Ellipsis], var2[Ellipsis]))
-                assert((var1[Ellipsis] == var2[Ellipsis]).all())
+                assert(np.allclose(var1[Ellipsis], var2[Ellipsis]))
+                #assert((var1[Ellipsis] == var2[Ellipsis]).all())
                 if verbose: print ("    Variable same")
             except AssertionError:
-                all_okay = False
-                failed_vars.append(var)
+                # Maybe it failed because its a masked array...
+                try:
+                    assert(np.allclose(var1[Ellipsis].data, var2[Ellipsis].data))
+                    assert(np.allclose(var1[Ellipsis].mask, var2[Ellipsis].mask))
+                except AssertionError:
+                    all_okay = False
+                    failed_vars.append(var)
+                    if verbose: print ("    Variable not same")
+                except:
+                    all_okay = False
+                    failed_vars.append(var)
+                    if verbose: print ("    Variable not same")
+
         else:
             if verbose: print ("    Skipping check for this variable")
 
     if not all_okay:
+        print(failed_vars)
         raise Exception("Variable verification failed")
 
 
@@ -75,11 +87,11 @@ def compare_attributes(ncf1, ncf2, verbose):
     assert_(att1, att2, "Attributes different in files")
     if verbose: 
         print("Comparing Attributes")
-        print("  Both files have {0} attributes".format(len(att1)))
+        print(("  Both files have {0} attributes".format(len(att1))))
 
     for att in att1:
         assert_(getattr(ncf1, att), getattr(ncf2, att), "Attribute {0} different in files".format(att))
-        if verbose: print("  Attribute {0} same".format(att))
+        if verbose: print(("  Attribute {0} same".format(att)))
 
 
 def compare(file1, file2, verbose):
